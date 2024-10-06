c7202c47b8dfd25a4547d0d87fee625f14242a17
Vignesh253 <142732211+Vignesh253@users.noreply.github.com>
2024-10-06 15:52:55 +0530

    Add files via upload

---
+++ dp.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+import numpy as np
